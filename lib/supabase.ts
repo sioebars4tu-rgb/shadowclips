0ee222fad26e83fb0aee6e411fb654c83b116179
unknown <tiktiktoktok00@gmail.com>
2026-07-21 21:00:04 +0700

    Hardcode Supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = https://nmeaifqvxgyzvwavijhb.supabase.co;
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tZWFpZnF2eGd5enZ3YXZpamhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzNzExODQsImV4cCI6MjA4NTk0NzE4NH0.tIzCj1YovFtHtb9LKIj_etp734BIk2vsqHUT3wB-54k;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
